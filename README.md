31d321861ff7efb1ad2586e3a7d237d646508be2
Robert Tierie <tangodolphin13@gmail.com>
2020-09-11 19:53:09 +0200

    done

--- README.md
+++ README.md
@@ -23,9 +23,25 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. How would you describe acessibility on the web to someone new to programming?
+
+Accessibility is extremely important as it allows websites to have a bigger audience and make use of that audience. if there is no mobile accessiblity you lose many customers who view the website from the mobile. 
+
+
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+
+1. Accompany multiple types of devices
+2. Use different layouts to make things easier for a particular device
+3. Add bigger texts for those who have seeing problems
+
+
 3. How would you explain the concept of a variable to someone new to programming?
+
+Variables are useful because they are ways to store data. Variables are extremely useful because these stored data would be easily reusable later on. 
+
+
 4. What is the purpose of using functions in code?
+
+A function is used to activate a particular action in a code. It's circumstancial and therefore be used when it is invoked and therefore useful as such. It is like the gas pedal in a car - when pressed it accelerates. 
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
--- index.js
+++ index.js
@@ -208,11 +208,15 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0]);
 
+console.log(artists[2].bio)
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[9].name = "Vincent van Gogh"
 
+console.log(artists[9]);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,19 +227,30 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+  return `The artist at index ${index} is ${array[index].name}`
+    
   }
+
+  
+console.log(getArtistByIndex(artists, 0))
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
 
-  /* Code here */
-
+const arArr = [];
+function get20s() {
+for (i = 0; i <= artists.length-1; i++) {
+  if(artists[i].bio == '19'){
+    arArr.push(artists[i].name);
+  }
 }
+console.log(arArr);
+}
+
+
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,9 +263,21 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
+function removeArtist(array, num) {
+  array.splice(num, 1);
+  return artists
+  console.log(array)
+
+
+
+
+
     /* code here */
   }
+
+
+    /* code here */
+
   
  
 
@@ -267,11 +294,13 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
+function addArtist(id, name, years, genre, nationality, bio){
+  artists.push({id, name, years, genre, nationality, bio});
+  return artists
 
   }
+
+  addArtist(21, "Robert Tierie", "1996 - current day", "web design", "Filipino", "lorem ipsum")
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,9 +310,14 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
+  const lots = [];
+  for (i = 0; i <= artists.length-1; i++) {
+    if(artists[i].paintings > 100){
+      lots.push(artists[i].name);
+    }
+  }
+  console.log(lots);
 
-  /* Code here */
 
 }
 
